5184c3b614e13d9cb81c41c554e1c28986b3b7b6
OlegPlugaru <olegplugaru.md@gmail.com>
2024-08-21 21:14:03 +0300

    Initial Commit

---
+++ Outline_DisclosureGroup/Outline_DisclosureGroupApp.swift
@@ -0,0 +1,17 @@
+//
+//  Outline_DisclosureGroupApp.swift
+//  Outline_DisclosureGroup
+//
+//  Created by Oleg Plugaru on 8/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Outline_DisclosureGroupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
